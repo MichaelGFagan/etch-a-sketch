dc3f73e4525bee23a00fc031f3d4c7e65e102f77
Michael Fagan <michael.g.fagan@gmail.com>
2023-04-14 08:45:17 -0500

    Finish assignment

--- js.js
+++ js.js
@@ -1,11 +1,41 @@
 const gridContainer = document.querySelector('#grid-container');
-for (i = 0; i < 16; i++) {
-    const divRow = document.createElement('div');
-    for (j = 0; j < 16; j++) {
+
+function setGrid(gridSize = 16) {
+    currentSquares = document.querySelectorAll('.square');
+    currentSquares.forEach((currentSquare) => {
+        gridContainer.removeChild(currentSquare);
+    })
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, auto)`
+    for (i = 0; i < gridSize ** 2; i++) {
         const divSquare = document.createElement('div');
         divSquare.className = 'square';
-        divSquare.textContent = 'Test';
-        divRow.appendChild(divSquare);
-    }
-    gridContainer.appendChild(divRow);
-};
+        gridContainer.appendChild(divSquare);
+    };
+    draw();
+}
+
+function updateBackground(e) {
+    e.target.style.background = 'black';
+    console.log(e);
+}
+
+function draw() {
+    const gridSquares = document.querySelectorAll('.square');
+    gridSquares.forEach((gridSquare) => {
+        gridSquare.addEventListener('mouseover', updateBackground);
+    });
+}
+
+
+function resetGrid() {
+    let gridSize = prompt('Enter grid square length: ');
+    if (gridSize > 100) {
+        gridSize = 100;
+    };
+    setGrid(gridSize);
+}
+
+let button = document.querySelector('button');
+button.addEventListener('click', resetGrid);
+
+setGrid();
